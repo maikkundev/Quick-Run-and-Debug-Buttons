62e796d897ea11da153c6d4540b75e4b3136f1c3
Michael Avagianos <maikkundv@proton.me>
2024-01-01 17:21:46 +0200

    Fix debug button command

--- src/extension.ts
+++ src/extension.ts
@@ -4,14 +4,14 @@ export function activate(context: vscode.ExtensionContext) {
   let debugButton = vscode.commands.registerCommand(
     "quick-run-debug-buttons.debugButton",
     () => {
-      vscode.commands.executeCommand("workbench.action.debug.run");
+      vscode.commands.executeCommand("workbench.action.debug.start");
     }
   );
 
   let runButton = vscode.commands.registerCommand(
     "quick-run-debug-buttons.runButton",
     () => {
-      vscode.commands.executeCommand("workbench.action.debug.start");
+      vscode.commands.executeCommand("workbench.action.debug.run");
     }
   );
 
