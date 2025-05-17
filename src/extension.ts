76ddd3ba5a45b7752549112a19b6ab2da3c506a8
Michael Avagianos <mixalisava@gmail.com>
2025-05-17 17:10:50 +0300

    feat: simplify registration for debug and run actions command

--- src/extension.ts
+++ src/extension.ts
@@ -1,74 +1,15 @@
-import * as vscode from "vscode";
+import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-  let config = vscode.workspace.getConfiguration("quick-run-debug-buttons");
-  let debugButtonAction = config.get("debugButton");
-  let runButtonAction = config.get("runButton");
+  let debugCommand = vscode.commands.registerCommand('quick-run-debug-buttons.debug', () => {
+    vscode.commands.executeCommand('workbench.action.debug.start');
+  });
+  context.subscriptions.push(debugCommand);
 
-  vscode.commands.executeCommand(
-    "setContext",
-    "debugButtonAction",
-    debugButtonAction
-  );
-  vscode.commands.executeCommand(
-    "setContext",
-    "runButtonAction",
-    runButtonAction
-  );
-
-  let debugButtonDebug = vscode.commands.registerCommand(
-    "quick-run-debug-buttons.debugButtonDebug",
-    () => {
-      vscode.commands.executeCommand("workbench.action.debug.start");
-    }
-  );
-  context.subscriptions.push(debugButtonDebug);
-
-  let debugButtonRun = vscode.commands.registerCommand(
-    "quick-run-debug-buttons.debugButtonRun",
-    () => {
-      vscode.commands.executeCommand("workbench.action.debug.run");
-    }
-  );
-  context.subscriptions.push(debugButtonRun);
-
-  let runButtonDebug = vscode.commands.registerCommand(
-    "quick-run-debug-buttons.runButtonDebug",
-    () => {
-      vscode.commands.executeCommand("workbench.action.debug.start");
-    }
-  );
-  context.subscriptions.push(runButtonDebug);
-
-  let runButtonRun = vscode.commands.registerCommand(
-    "quick-run-debug-buttons.runButtonRun",
-    () => {
-      vscode.commands.executeCommand("workbench.action.debug.run");
-    }
-  );
-  context.subscriptions.push(runButtonRun);
-
-  context.subscriptions.push(
-    vscode.workspace.onDidChangeConfiguration((e) => {
-      if (
-        e.affectsConfiguration("quick-run-debug-buttons.debugButton") ||
-        e.affectsConfiguration("quick-run-debug-buttons.runButton")
-      ) {
-        vscode.window
-          .showInformationMessage(
-            "Configuration changed, please restart the extension for changes to take effect.",
-            "Restart"
-          )
-          .then((selection) => {
-            if (selection === "Restart") {
-              vscode.commands.executeCommand(
-                "workbench.action.restartExtensionHost"
-              );
-            }
-          });
-      }
-    })
-  );
+  let runCommand = vscode.commands.registerCommand('quick-run-debug-buttons.run', () => {
+    vscode.commands.executeCommand('workbench.action.debug.run');
+  });
+  context.subscriptions.push(runCommand);
 }
 
 export function deactivate() {}
